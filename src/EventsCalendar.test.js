116b0cdce2d7f5b4ea294ebd2d007c5229c5cc05
Przemyslaw Krawczyk <przemekk437@gmail.com>
2020-02-19 19:30:28 +0100

    initial app commit

--- src/EventsCalendar.test.js
+++ src/EventsCalendar.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import EventsCalendar from './EventsCalendar';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<EventsCalendar />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
