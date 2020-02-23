403819542c1472acc12a2a372ab67f5011869bd9
Przemyslaw Krawczyk <przemekk437@gmail.com>
2020-02-23 22:34:08 +0100

    fix bug with mutating state

--- src/Calendar.js
+++ src/Calendar.js
@@ -5,39 +5,31 @@ import Form from './components/Form'
 
 class Calendar extends Component {
   state = {
-    events: [
-      {
-        id: 0,
-        title: '',
-        date: '',
-        time: '',
-        description: ''
-      }
-    ]
+    eventInfo: {
+      id: 0,
+      title: '',
+      date: '',
+      time: '',
+      description: ''
+    },
+    events: []
   }
 
   handleChange = ({ target: { name, value } }) => {
-    const getInputValues = this.state.events.map(event => {
-      const copiedEvent = {...event}
-      copiedEvent[name] = value
-      return copiedEvent
-    })
-    this.setState((state) => ({
-      ...state,
-      events: getInputValues
-    }), () => console.log(this.state.events))
+    const { id } = this.state.eventInfo
+    const data = { ...this.state.eventInfo, id: id + 1 , [name]: value}
+    this.setState(() => ({
+      eventInfo: data,
+      events: [...this.state.events, data]
+    }), () => console.log(this.state))
   }
 
   handleSubmit = event => {
     event.preventDefault();
-    // const getLastId = this.state.events.map(event => {
-    //   const copiedEvent = {...event}
-    //   copiedEvent.id += 1
-    //   return copiedEvent
-    // })
-    // localStorage.setItem('events', JSON.stringify(events))
-    console.log(this.state.events)
+    const { events } = this.state
+    localStorage.setItem('events', JSON.stringify(events))
   }
+
 render() {
   return (
     <div className="calendar">
