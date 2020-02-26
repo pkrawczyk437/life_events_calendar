0aa78f7857f577d2b5c53d8e0708375f36a2288e
Przemyslaw Krawczyk <przemekk437@gmail.com>
2020-02-26 21:41:18 +0100

    add handling input values and add localStorage

--- src/Calendar.js
+++ src/Calendar.js
@@ -15,30 +15,45 @@ class Calendar extends Component {
     events: []
   }
 
+  componentDidMount() {
+    const events = JSON.parse(localStorage.getItem("events")) || [];
+      this.setState(() => ({
+        events
+      }))
+  }
+
   handleChange = ({ target: { name, value } }) => {
-    const { id } = this.state.eventInfo
-    const data = { ...this.state.eventInfo, id: id + 1 , [name]: value}
-    this.setState(() => ({
-      eventInfo: data,
-      events: [...this.state.events, data]
-    }), () => console.log(this.state))
+      const data = { ...this.state.eventInfo, [name]: value}
+      this.setState(() => ({
+        eventInfo: data,
+      }))
   }
 
   handleSubmit = event => {
-    event.preventDefault();
-    const { events } = this.state
-    localStorage.setItem('events', JSON.stringify(events))
+    event.preventDefault()
+    const data = {...this.state.eventInfo}
+    const { id } = data
+    const updatedEvent = {...data, id: id + 1 }
+
+    this.setState(() => ({
+      eventInfo: updatedEvent,
+      events: [...this.state.events, updatedEvent]
+    }), () => {
+      localStorage.setItem('events', JSON.stringify(this.state.events))
+    })
   }
 
 render() {
   return (
     <div className="calendar">
       <Form handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+      {this.state.events.length > 0 &&
         <div className="calendar-container">
           {this.state.events.map( event => (
             <Event {...event} key={event.id}/>
           ))}
         </div>
+      }
     </div>
   )
  }
