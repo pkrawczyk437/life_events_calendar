3bb4a6125874e2f084f38ebcb14e1cf3339a2ae2
Przemyslaw Krawczyk <przemekk437@gmail.com>
2020-03-04 20:59:54 +0100

    Add responsive form with validation alerts

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,19 +5,27 @@ const Form = ({handleChange, handleSubmit, errors}) => {
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name your event..." name="title" onChange={handleChange}/>
-        {title.length > 0 &&
-          <div className="error">{title}</div>
-        }
-        <input type="date" placeholder="Add date of it..." name="date" onChange={handleChange}/>
-        {date.length > 0 &&
-          <div className="error">{date}</div>
-        }
-        <input type="time" name="time" placeholder="Add time for them..." onChange={handleChange}/>
-        {time.length > 0 &&
-          <div className="error">{time}</div>
-        }
-        <input type="submit" value="Add event"/>
+        <div className="dataInput">
+          <input type="text" placeholder="Name your event..." name="title" onChange={handleChange}/>
+          {title.length > 0 &&
+            <div className="error">{title}</div>
+          }
+        </div>
+        <div className="dataInput">
+          <input type="date" placeholder="Add date of it..." name="date" onChange={handleChange}/>
+          {date.length > 0 &&
+            <div className="error">{date}</div>
+          }
+        </div>
+        <div className="dataInput">  
+          <input type="time" name="time" placeholder="Add time for them..." onChange={handleChange}/>
+          {time.length > 0 &&
+            <div className="error">{time}</div>
+          }
+        </div>
+        <div className="submitInput">
+          <input type="submit" value="Add event"/>
+        </div>  
       </form>
     </div>
   )
