c494d2eea01960632977bdcc4eaae8b45befdeee
Przemyslaw Krawczyk <przemekk437@gmail.com>
2020-03-21 23:03:18 +0100

    Updates

---
+++ precache-manifest.153fc0e512a60ceefe3f63761b82b1c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f17a8ef10718e2ca8e655d58a86047",
+    "url": "/life_events_calendar/index.html"
+  },
+  {
+    "revision": "afef3a14608bd6dd3484",
+    "url": "/life_events_calendar/static/css/main.1254b194.chunk.css"
+  },
+  {
+    "revision": "126098d6370cc0a1116e",
+    "url": "/life_events_calendar/static/js/2.00f68a6b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/life_events_calendar/static/js/2.00f68a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afef3a14608bd6dd3484",
+    "url": "/life_events_calendar/static/js/main.f733aa4b.chunk.js"
+  },
+  {
+    "revision": "bc309c6f84fd8dc8015a",
+    "url": "/life_events_calendar/static/js/runtime-main.e3e62d51.js"
+  },
+  {
+    "revision": "80c3bc10aa1fdbc426f8417ba90c9cdc",
+    "url": "/life_events_calendar/static/media/background.80c3bc10.png"
+  }
+]);
